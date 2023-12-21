0cc677f4e995f8dc41f29c645e6f1ad0c9187a6c
Mohammed Irtisam Sajin <86884797+IrtisamSajin@users.noreply.github.com>
2023-12-21 20:02:06 +0600

    Update

--- server/Routes/signupRoute.js
+++ server/Routes/signupRoute.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const validator=require('validator');
-const bcrypt=require('bcrypt')
+const bcrypt=require('bcryptjs')
 const User=require('../Models/usersModel');
 const { getToken } = require('../Helpers/tokens');
 
